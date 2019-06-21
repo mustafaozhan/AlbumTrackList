59aaa0afcec3b1c2fb1be1de8b44dc3c7a1fef18
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-06-21 22:05:35 +0200

    Initial Commit

---
+++ AlbumTrackList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlbumTrackList
+//
+//  Created by Mustafa Ozhan on 21/06/2019.
+//  Copyright © 2019 Mustafa Ozhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
